cd3be58075773602c5edaa62c29d8c337bec36d0
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-05-21 07:27:41 +0300

    Time: 2350 ms (75.36%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,7 +1,16 @@
 SELECT customer_id, COUNT(customer_id) AS count_no_trans 
+FROM Visits as v LEFT JOIN Transactions as t 
+ON v.visit_id = t.visit_id 
+WHERE t.transaction_id IS NULL 
+GROUP BY customer_id;
+
+/*
+-- Solution using subquery
+SELECT customer_id, COUNT(customer_id) AS count_no_trans 
 FROM Visits 
 WHERE visit_id NOT IN (
-    SELECT  visit_id 
+    SELECT visit_id 
     FROM Transactions
 )
-group by customer_id;
+GROUP BY customer_id;
+*/
