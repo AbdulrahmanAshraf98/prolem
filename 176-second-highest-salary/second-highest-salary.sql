7fe7e41dbeda87b4ebd81de9e6826eb84bb662aa
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-05-22 08:32:29 +0300

    Time: 434 ms (82.01%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
 select 
-case when count(id)>1 then (select distinct  salary from employee  order by salary desc limit 1 offset 1  ) 
-else null end as  SecondHighestSalary 
-from employee 
+(select distinct  salary from employee  order by salary desc limit 1 offset 1  ) 
+ as  SecondHighestSalary 
