ad1e1d662f1a9048c53b943e39dc029bc56ee321
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-05-27 07:35:57 +0300

    Time: 1190 ms (39.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+select d.name  as  Department ,e.name as Employee  , e.salary  as Salary 
+from (
+    select * , dense_rank() over(partition by departmentId   order by  salary desc)as dr 
+    from Employee as e1
+)as e 
+join Department as d 
+on e.departmentId =d.id 
+where e.dr=1
