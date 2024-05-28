27644dc3a48260584c5972d7231bf85b7139649d
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-05-29 02:30:56 +0300

    Time: 1112 ms (30.11%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,11 +1,11 @@
 /* Write your T-SQL query statement below */
- select distinct  employee_id from Employees 
+ select   employee_id from Employees 
  where employee_id not in (
     select distinct employee_id  
     from Salaries 
  )
- union all 
- select distinct employee_id from Salaries 
+ union  
+ select  employee_id from Salaries 
  where employee_id not in (
      select distinct employee_id  
     from Employees 
