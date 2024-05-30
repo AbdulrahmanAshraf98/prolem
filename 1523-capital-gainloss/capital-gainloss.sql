c53a876e1f369cfce700ac4be79162cbef0782c5
Abdulrhman Ashraf <89071774+AbdulrahmanAshraf98@users.noreply.github.com>
2024-05-30 04:59:14 +0300

    Time: 2130 ms (38.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select stock_name , sum(
+    case when operation = 'Sell' then price  
+    else -price end 
+)as capital_gain_loss 
+from Stocks 
+group by stock_name    
+order by capital_gain_loss desc
